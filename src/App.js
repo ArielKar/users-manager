471c5a1b3a936d053e83529997b4e9558e08b137
arielkar <arielkarmazin@gmail.com>
2020-05-24 16:01:57 +0300

    created layout and Header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Grid } from '@material-ui/core';
+import Header from './Components/Header';
 
 function App() {
-  return <div></div>;
+  return (
+    <Grid container direction="column">
+      <Grid item>
+        <Header />
+      </Grid>
+      <Grid item container>
+        <Grid xs={0} sm={2} />
+        <Grid item xs={12} sm={8}>
+          This is the main content
+        </Grid>
+        <Grid xs={0} sm={2} />
+      </Grid>
+    </Grid>
+  );
 }
 
 export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography, Box } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+import logo from '../assets/logo_coognitive.svg';
+
+const useStyles = makeStyles(() => ({
+  logoWrapper: {},
+  logo: {
+    height: '25px',
+  },
+  pageNameWrapper: {
+    borderTop: '1px solid #fff',
+  },
+  pageName: {
+    fontSize: '18px',
+    letterSpacing: '2px',
+  },
+}));
+
+const Header = () => {
+  const classes = useStyles();
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Box>
+          <Box className={classes.logoWrapper}>
+            <img src={logo} className={classes.logo} alt="company logo" />
+          </Box>
+          <Box className={classes.pageNameWrapper}>
+            <Typography className={classes.pageName}>Users Manager</Typography>
+          </Box>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
